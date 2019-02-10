e3a78d62af50f3b036f4ffe7c0af28ea863e8850
Vitalie Lupu <vitaliix@gmail.com>
2019-02-11 00:36:21 +0200

    Add gitignore file and also reformat header.php to echo all html content

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html lang="<?= get_bloginfo('language'); ?>">
-<head>
 <?php
 
 use wp\WidgetArea;
 use wp\WPUtils;
+use wp\WPSidebar;
+use wp\WidgetSiteBranding;
 
 if (is_home() || is_front_page()) {
     $pageTitle = get_the_title(get_option('page_for_posts', true));
@@ -19,43 +18,37 @@ if (is_category() || is_tax() || is_tag()) {
 } else {
     $pageTitle = get_the_title();
 }
-$blogName = get_option('blogname');
-echo "<title>{$pageTitle} - {$blogName}</title>";
-?>
-<meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?>">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="format-detection" content="telephone=no">
-<?php
+$siteName = get_bloginfo('name', 'display');
+$siteLanguage = get_bloginfo('language');
+$siteCharset = get_bloginfo('charset', 'display');
+//TODO Check if urlSiteIcon is empty then apply theme site icon
 $urlSiteIcon = get_site_icon_url();
-if ($urlSiteIcon) {
-    echo "<link rel='apple-touch-icon' href='{$urlSiteIcon}'>\n<link rel='shortcut icon' href='{$urlSiteIcon}'>\n";
+$contentHead = WPUtils::doAction('wp_head');
+$bodyClasses = join(' ', get_body_class());
+$content = '';
+$content .= WPUtils::getSidebarContent(WidgetArea::HEADER_TOP);
+$content .= WPUtils::getSidebarContent(WidgetArea::HEADER_MAIN);
+$content .= WPUtils::getSidebarContent(WidgetArea::HEADER_BOTTOM);
+if (empty($content)) {
+    ob_start();
+    the_widget(WidgetSiteBranding::class, [], [
+        WPSidebar::BEFORE_WIDGET => '<div class="widget %s text-xs-center">'
+    ]);
+    $content = WPUtils::getSidebar(WidgetArea::HEADER_TOP, ob_get_clean());
 }
-wp_head();
-?>
+echo "<!DOCTYPE html><html lang='{$siteLanguage}'>
+<head>
+    <title>{$siteName} - {$pageTitle}</title>
+    <meta charset='{$siteCharset}'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
+    <meta name='apple-mobile-web-app-title' content='{$siteName}'>
+    <meta name='apple-mobile-web-app-status-bar-style' content='black'>
+    <meta name='apple-mobile-web-app-capable' content='yes'>
+    <meta name='format-detection' content='telephone=no'>
+    <link rel='apple-touch-icon' href='{$urlSiteIcon}'>
+    <link rel='shortcut icon' href='{$urlSiteIcon}'>
+    {$contentHead}
 </head>
-<body <?php body_class(); ?>>
-<header>
-    <?php
-
-    use wp\WPSidebar;
-    use wp\WidgetSiteBranding;
-
-    $content = '';
-    $content .= WPUtils::getSidebarContent(WidgetArea::HEADER_TOP);
-    $content .= WPUtils::getSidebarContent(WidgetArea::HEADER_MAIN);
-    $content .= WPUtils::getSidebarContent(WidgetArea::HEADER_BOTTOM);
-    if (empty($content)) {
-        ob_start();
-        the_widget(WidgetSiteBranding::class, [], [
-            WPSidebar::BEFORE_WIDGET => '<div class="widget %s text-xs-center">'
-        ]);
-        echo WPUtils::getSidebar(WidgetArea::HEADER_TOP, ob_get_clean());
-    } else {
-        echo $content;
-    }
-    ?>
-</header>
-<main>
+<body class='{$bodyClasses}'>
+<header>{$content}</header>
+<main>";
